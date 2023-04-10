b830d0cc46aab30b09a0eb699c7daf72ccf45573
ÿiminchen1999 <yc4195@cumc.columbia.edu>
2023-04-10 16:54:34 -0400

    update plot

--- test for difference among 12 students.py
+++ test for difference among 12 students.py
@@ -7,18 +7,18 @@ import seaborn as sns
 all_scores_df = pd.read_excel("2022_frank_sentiment_scores.xlsx")
 
 # Define the color palette for each sentiment
-#colors = ["#E74C3C", "#95A5A6", "#2ECC71", "#3498DB"]
-colors = ["#E74C3C", "#95A5A6", "#2ECC71"]
+colors = ["#E74C3C", "#95A5A6", "#2ECC71", "#3498DB"]
+#colors = ["#E74C3C", "#95A5A6", "#2ECC71"]
 
 # Set the plot style
 sns.set_style("ticks")
 sns.set_context("talk")
 
 # Melt the dataframe to long format
-#melted_df = all_scores_df.melt(id_vars=["File", "Sentence"], value_vars=["Negative", "Neutral", "Positive", "Compound"], var_name="Sentiment")
+melted_df = all_scores_df.melt(id_vars=["File", "Sentence"], value_vars=["Negative", "Neutral", "Positive", "Compound"], var_name="Sentiment")
 
 #ignore compound
-melted_df = all_scores_df.melt(id_vars=["File", "Sentence"], value_vars=["Negative", "Neutral", "Positive"], var_name="Sentiment")
+#melted_df = all_scores_df.melt(id_vars=["File", "Sentence"], value_vars=["Negative", "Neutral", "Positive"], var_name="Sentiment")
 #, "Compound"],
 # Create the boxplot
 fig, ax = plt.subplots(figsize=(12, 8))
@@ -30,7 +30,8 @@ plt.xlabel("Student Number ", fontsize=16)
 plt.ylabel("Sentiment Score", fontsize=16)
 sns.despine(offset=10, trim=True)
 plt.tight_layout()
+plt.savefig("folder for plot/2022_compound_frank_sentiment_analysis_scores.png")
 plt.show()
-plt.savefig("2022_ver2_frank_sentiment_analysis_scores.png")
+
 
 
