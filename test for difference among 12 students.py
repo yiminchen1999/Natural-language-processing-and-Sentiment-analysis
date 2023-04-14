dbebee65802a9be2baf6978ca4e0d58fa3c1128a
ÿiminchen1999 <90345634+yimin1019@users.noreply.github.com>
2023-04-14 15:06:56 -0400

    update plot

--- test for difference among 12 students.py
+++ test for difference among 12 students.py
@@ -22,19 +22,22 @@ fig, ax = plt.subplots()
 sns.scatterplot(data=data, x='file_number', y='score', hue='label', palette=label_colors)
 
 # Set the axis labels and title
+# Set the x-axis ticks
+plt.xticks(range(1, 13), range(1, 13))
+
 plt.xlabel('File Number')
 plt.ylabel('Score')
 plt.title('Sentiment Analysis Results')
 
 # Create a boxplot of the score for each file number and label
-sns.boxplot(data=data, x='file_number', y='score', hue='label', palette=label_colors, width=0.4, whis=1.5, dodge=True)
+#sns.boxplot(data=data, x='file_number', y='score', hue='label', palette=label_colors, width=0.4, whis=1.5, dodge=True)
 
 # Set the legend outside the plot and adjust spacing
 plt.legend(bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0.)
 plt.subplots_adjust(right=0.8)
 
 # Save the plot
-plt.savefig("folder for plot/2022_ver3_frank_sentiment_analysis_scores.png")
+plt.savefig("folder for plot/2022_ver5_frank_sentiment_analysis_scores.png")
 
 # Display the plot
 plt.show()
